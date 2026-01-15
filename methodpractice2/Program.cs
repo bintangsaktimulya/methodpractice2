95a6ad469768650924c2b5b5efffb1f6af8f0da4
Bintang Sakti Mulya <bintangsaktimulya@gmail.com>
2026-01-15 15:24:21 +0700

    non void

--- methodpractice2/Program.cs
+++ methodpractice2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double result  = 0;
+
+double circle(double radius, double pi= 3.14)
+{
+    return pi * radius * radius;
+}
+
+double square(double side)
+{
+    return side * side;
+}
+
+double rectangle (double width, double height)
+{
+    return width * height ;
+}
+
+double triangle(double basse, double height)
+{
+    return basse * height/2;
+}
+
+Console.WriteLine($"the result of the circle {circle(3)}");
+Console.WriteLine($"the result of the square {square(5)}");
+Console.WriteLine($"the result of the rectangle {rectangle(4, 6)}");
+Console.WriteLine($"the result of the triangle {triangle(9, 6)}");
